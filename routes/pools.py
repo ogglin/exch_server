29d76c9664e98e363ab98e27406672eeec107a49
ares <server.ares@gmail.com>
2023-02-20 20:18:47 +0300

    init v1

--- routes/pools.py
+++ routes/pools.py
@@ -13,15 +13,18 @@ router = APIRouter()
 async def pools(pool: str = None):
     items = None
     if pool:
-        items = {}
-        if pool == 'pools-v2' or pool == 'pools-v3':
-            for k, v in (await old_redis.hgetall(pool)).items():
-                items[k] = json.loads(v.replace('True', 'true').replace('False', 'false'))
+        # items = {}
+        if pool == 'pools-v2' or pool == 'pools-v3' or pool == 'sushi_v2_pools':
+            items = await old_redis.hgetall(pool)
+            # for k, v in (await old_redis.hgetall(pool)).items():
+            #     items[k] = json.loads(v.replace('True', 'true').replace('False', 'false'))
         else:
-            for k, v in (await redis.hgetall(pool)).items():
-                items[k] = json.loads(v)
-    if items is None:
-        return JSONResponse(status_code=404, content={"message": f"Not found {pool}"})
+            items = await redis.hgetall(pool)
+            # for k, v in (await redis.hgetall(pool)).items():
+            #     items[k] = json.loads(v)
+    if items is None or len(items) < 1:
+        return JSONResponse(status_code=404, content={
+            "message": f"Не найден {pool}, примеры: pools-v2, pools-v3, pools_pancake, sushi_v2_pools"})
     return {pool: items}
 
 
@@ -29,11 +32,12 @@ async def pools(pool: str = None):
 async def replica(market: str = None):
     items = None
     if market:
-        items = {}
-        for k, v in (await redis.hgetall(market)).items():
-            items[k] = json.loads(v)
-    if items is None:
-        return JSONResponse(status_code=404, content={"message": f"Not found {market}"})
+        items = await redis.hgetall(market)
+        # for k, v in (await redis.hgetall(market)).items():
+        #     items[k] = json.loads(v.replace("'", '"'))
+    if items is None or len(items) < 1:
+        return JSONResponse(status_code=404, content={
+            "message": f"Не найден {market}, примеры: ascendex, bitrue, bkex, gate, hitbtc, hotbit, kucoin, mxc"})
     return {market: items}
 
 
