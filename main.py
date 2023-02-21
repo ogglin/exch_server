2dee4911223d8150fe4987783b547be751c0f672
ares <server.ares@gmail.com>
2023-02-21 17:54:10 +0300

    init v1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
-from typing import Union
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse
 
 from routes import pools
+from routes.websock import *
 
 app = FastAPI()
 
@@ -28,7 +28,24 @@ def read_root():
     return HTMLResponse(content=html_content, status_code=200)
 
 
+@app.websocket("/ws/")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client left the chat")
+
+
 async def run_server():
+    asyncio.create_task(timers())
+    asyncio.create_task(settings())
+    asyncio.create_task(last_block())
+    asyncio.create_task(profits())
     config = uvicorn.Config("main:app", port=3080, host='0.0.0.0', log_level="info", reload=True)
     server = uvicorn.Server(config)
     await server.serve()
---
+++ routes/websock.py
@@ -0,0 +1,69 @@
+import asyncio
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+from db import *
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+async def timers():
+    while True:
+        try:
+            items = await old_redis.hgetall('timers')
+            await manager.broadcast(f'"timers": {items}')
+            await asyncio.sleep(1)
+        except Exception as err:
+            print('ws', err)
+
+
+async def settings():
+    while True:
+        try:
+            items = await redis.get('settings')
+            await manager.broadcast(f'"settings": {items}')
+            await asyncio.sleep(1)
+        except Exception as err:
+            print('ws', err)
+
+
+async def last_block():
+    while True:
+        try:
+            items = await redis.get('last_block')
+            await manager.broadcast(f'"last_block": {items}')
+            await asyncio.sleep(1)
+        except Exception as err:
+            print('last_block', err)
+
+
+async def profits():
+    while True:
+        try:
+            items = await redis.hgetall('profits')
+            await manager.broadcast(f'"profits": {items}')
+            await asyncio.sleep(1)
+        except Exception as err:
+            print('profits', err)
